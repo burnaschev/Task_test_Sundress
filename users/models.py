fc5a1e32cd5179ec8944791938bb95cae44c012c
Артур <informal201144@gmai;.com>
2024-02-28 16:49:33 +0500

    added users model and jwt/djoser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, first_name, last_name, phone, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+        )
+        user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
+        user = self.create_user(
+            email,
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            password=password,
+
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
+    username = None
+
+    first_name = models.CharField(max_length=50, verbose_name='имя')
+    last_name = models.CharField(max_length=50, verbose_name='фамилия')
+
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    image = models.ImageField(upload_to='users_img/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', "role"]
+
+    objects = UserManager()
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path('users/api/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
